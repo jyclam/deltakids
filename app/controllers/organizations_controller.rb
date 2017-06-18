4bff90c41a5b8dcaa217480b5474feba6fe0fed6
Michael Luo <michaelluo98@gmail.com>
2017-06-17 18:30:48 -0700

    Unsuccessfully implement create action in organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,7 +4,39 @@ class OrganizationsController < ApplicationController
   end
 
   def create 
-	render json: params
+	#render json: params
+	@organization = Organization.new(organ_params)
+	if @organization.save
+	  # session[:org_id] = @organization.id
+	  redirect_to home_path 
+	else 
+	  render :new 
+	end
   end
+
+  private 
+
+  def organ_params 
+	params.require(:organization).permit(
+	  :title, 
+	  :street_address, 
+	  :unit_num, 
+	  :postal_code, 
+	  :city, 
+	  :age_group, 
+	  :activity_type, 
+	  :description,
+	  :target_clientelle, 
+	  :services, 
+	  :phone_num, 
+	  :website, 
+	  :email,
+	  :first_name, 
+	  :last_name, 
+	  :password, 
+	  :password_confirmation
+	)
+  end
+
 
 end
