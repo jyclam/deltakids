7ae577f8de36dc1524a1e7e7b39631792cc1ca82
Celina Gentil <celinagentil@gmail.com>
2017-06-18 09:52:16 -0700

    Create background view, main filter program, add bootstrap styling gem, jquery UI gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170617234722) do
-=======
 ActiveRecord::Schema.define(version: 20170618000351) do
->>>>>>> integration
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
