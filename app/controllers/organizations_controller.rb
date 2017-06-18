48cd3c661732d468c2403dd914211378009c96b2
Michael Luo <michaelluo98@gmail.com>
2017-06-17 20:48:33 -0700

    Fix services association dropdown menu

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,12 +1,13 @@
 class OrganizationsController < ApplicationController
   def new
+	 @services = ['Help in A Crisis', 'Assistance with Fees', 'Baby and Health Development','Child and Health Development', 'Child Advocacy', 'Childcare and Preschool', 'Support for Children', 'Support for families']
 	@organization = Organization.new
   end
 
   def create
-	#render json: params
-  byebug
 	@organization = Organization.new(organ_params)
+  # @organization.service_ids = [params[:organization][:service_ids]]
+
 	if @organization.save
 	  # session[:org_id] = @organization.id
 	  redirect_to home_path
@@ -19,7 +20,8 @@ class OrganizationsController < ApplicationController
 
   def organ_params
 	params.require(:organization).permit(
-    {:programs_offered_ids => []},
+    {programs_offered_ids: []},
+    :service_ids,
 	  :title,
 	  :street_address,
 	  :unit_num,
