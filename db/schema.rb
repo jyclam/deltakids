087009c4ce7da7a2207a4306e3144c96081b74aa
Michael Luo <michaelluo98@gmail.com>
2017-06-18 12:09:27 -0700

    Add cost to activities table and add to form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618182521) do
+ActiveRecord::Schema.define(version: 20170618190637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170618182521) do
     t.time "time_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cost"
     t.index ["age_group_id"], name: "index_activities_on_age_group_id"
     t.index ["organization_id"], name: "index_activities_on_organization_id"
     t.index ["program_id"], name: "index_activities_on_program_id"
