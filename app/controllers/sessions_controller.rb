4b7a8231c5dc91b5d0734065748f2544ba2890d7
Michael Luo <michaelluo98@gmail.com>
2017-06-18 10:21:32 -0700

    Add ability for organizations to sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,17 @@ class SessionsController < ApplicationController
 	organization = Organization.find_by(email: params[:email])
 
 	if organization&.authenticate(params[:password]) 
-	  sessions[:org_id] = organization.id
+	  session[:org_id] = organization.id
 	  redirect_to home_path 
 	else 
 	  render :new 
 	end
-
   end
+
+  def destroy
+	session[:org_id] = nil 
+	redirect_to home_path
+  end
+
 
 end
