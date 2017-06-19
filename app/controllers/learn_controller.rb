5d0c39f75d54b232f744de442157673b68e9f181
Spencer Cheung <spencercheung1@gmail.com>
2017-06-18 19:42:36 -0700

    added 5210live page

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -6,7 +6,8 @@ class LearnController < ApplicationController
 
 
   def live
-    render json: { hello: 'live_5_2_1_0' }
+    # render json: { hello: 'live_5_2_1_0' }
+    # render learn_live_url;
   end
 
 end
