27fcb3146eb8b6aa7c1c80a9cc3167b43824e8f4
SafiraLiteng <celinagentil@gmail.com>
2017-06-19 05:59:26 -0700

    did a lot of work (#65)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
   def org_signed_in?
 	if session[:org_id].present? && current_org.nil?
 	  session[:org_id] = nil
@@ -11,7 +12,7 @@ class ApplicationController < ActionController::Base
   helper_method :org_signed_in?
 
   def current_org
-	   @organization ||= Organization.find_by(id: session[:org_id])
+	@organization ||= Organization.find_by(id: session[:org_id])
   end
 
   helper_method :current_org
